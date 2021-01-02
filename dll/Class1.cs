3a10d98f31ffc9483ced9373c951efbfe9cd7388
k <k@klabz.org>
2021-01-02 15:54:03 +0100

    Forgot to save the file :D

--- dll/Class1.cs
+++ dll/Class1.cs
@@ -1,12 +1,10 @@
 using System;
 
-namespace dll
-{
-    public class Class1
-    {
-		/* [MyAttr(null)] */
+namespace dll {
+	public class Class1 {
+		[MyAttr(null)]
 		public static int test;
-    }
+	}
 
 	class MyAttrAttribute : Attribute {
 		public MyAttrAttribute(System.Type t) {}
