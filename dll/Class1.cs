ff09a5e74ce2bdc1023a956d7eada77ed46e4908
k <k@klabz.org>
2021-01-02 15:42:17 +0100

    Basic repro

---
+++ dll/Class1.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace dll
+{
+    public class Class1
+    {
+		/* [MyAttr(null)] */
+		public static int test;
+    }
+
+	class MyAttrAttribute : Attribute {
+		public MyAttrAttribute(System.Type t) {}
+	}
+}
